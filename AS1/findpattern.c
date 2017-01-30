7ad2dfc3b446f9a4cb54c3771d6f913218c757b4
justinwidney <jwidney@ualberta.ca>
2017-01-30 16:35:36 -0700

    more work on function

--- AS1/findpattern.c
+++ AS1/findpattern.c
@@ -11,19 +11,48 @@
 #define MEM_RO 1
 
 
+jmp_buf env;
+
 struct patmatch {
 	unsigned int  location;
 	unsigned char mode; /* MEM_RW, or MEM_RO */
 };
 
 
+/* Handle Segfault
+   increment to next page */
+
+void seg_handler(int signo) {
+
+
+	siglongjmp(env,1);
+	address = address + PAGE_SIZE;
+}
 
 unsigned int findpattern (unsigned char *pattern, unsigned int patlength,
 struct patmatch *locations, unsigned int loclength) {
 
 int occcurances = 0 ; 
 
+void * address = 0x00000000
+print(&address)
 
+while(1){
+
+// try to read 
+
+
+// try to write
+
+sigsetjump(env,1);
+
+
+
+// set Mode
+
+
+
+}
 
 
 return occurances
@@ -32,7 +61,21 @@ return occurances
 
 int main(int argc, char ** argv) {
 
+struct sigaction seg_act;
+seg_act.sa_handler = seg_Handler;
+sigemptyset(&seg_act.sa_mask);
+
+sigaction(SIGSEGV, &seg_act, NULL);
+
 
 unsigned char* locations[]; 
+
+findpattern
+
+printf("ADDRESS:")
+printf("---MODE---")
+
+
+
 
 }
