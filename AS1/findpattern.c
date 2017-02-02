567039d53ad4e5eb29646e8616612923a8bfe405
Justin Widney <jwidney@cs.ualberta.ca>
2017-02-02 13:14:36 -0700

    signal handling progress

--- AS1/findpattern.c
+++ AS1/findpattern.c
@@ -1,50 +1,78 @@
 #include "findpattern.h"
+#include <unistd.h>
 
 jmp_buf unreadable_memory;
 jmp_buf readonly_memory;
 
-char * address = 0x00000000;
+char * address = (char*) 0x00000000;
 char mode = MEM_RO;
 
 /* Handle Segfault
    increment to next page */
 
 void seg_handler(int signo) {
-	if (mode == MEM_RO) {
-		address += getpagesize();
-		siglongjmp(unreadable_memory,1);
-	}
-	else {
-		siglongjmp(readonly_memory,1);
-	}
 	
+		siglongjmp(readonly_memory,1);
+		return;
 }
 
 unsigned int findpattern (unsigned char *pat, unsigned int patlength, struct patmatch *locations, unsigned int loclength) {
-struct sigaction seg_act;
+	
+	// set-up stuff
+	struct sigaction seg_act;
 	seg_act.sa_handler = seg_handler;
 	sigemptyset(&seg_act.sa_mask);
 	sigaction(SIGSEGV, &seg_act, NULL); 
 	
 	int occcurances = 0; 
 	char pattern = *pat;
-	
+	int sig_value;
 	int i = 0;
+	int prev_address;
+	char data; 
+	
+
 	while(1) {
-		sigsetjmp(readonly_memory,1);
-		mode = MEM_RO;
-		char * address = 0x0000000;
-		mode = MEM_RW;
-		char c = *address;
-		sigsetjmp(readonly_memory,1);
-		address += getpagesize();
 		
-		if (mode = MEM_RO) { printf("Readable memory at %p\n", address); }
-		else { printf("Read and Wrtie memory at %p\n", address); }
-		if (i >= 0xFFFFFFFF/getpagesize()) {
-			break;
+		
+		sig_value = sigsetjmp(readonly_memory,1);
+
+		if( sig_value == 0) {
+		data = *address;
+		
 		}
-		i++;
+		else {
+		//printf("Non Readale memory at %p\n", address);
+		address += getpagesize();
+		continue;
+		}
+
+		sig_value = sigsetjmp(readonly_memory,1);
+
+		if(sig_value == 0) {
+		
+		*address = 'w';
+		//printf("why?");
+		*address = data; 
+	
+		mode = MEM_RW;
+		//printf("why2?");
+
+		} else {
+		mode = MEM_RO;
+		//printf("why3?");
+		}
+		
+		// restore stuff 
+		//*address = data;
+		
+		
+		if (mode == MEM_RO) { printf("Readable memory at %p\n", address); }
+		else if (mode == MEM_RW) { printf("Read and Write memory at %p\n", address); }
+		address += getpagesize();
+
+		if(address == 0x00000000){ 
+		break;}
 	}
 	return occcurances;
 }
