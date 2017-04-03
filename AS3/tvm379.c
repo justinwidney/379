5833b594120743b711017f3c87e2f392fae3cccb
justinwidney <jwidney@ualberta.ca>
2017-04-03 13:52:24 -0600

    more work on assignment 3

--- AS3/tvm379.c
+++ AS3/tvm379.c
@@ -5,7 +5,107 @@
 #define FIFO 2
 #define LRU 3
 
+int* stack;
+int gp_Mode, fl_Mode;
 
+int TLB_top = -1;   // for stacks
+int PT_Top = -1;
+
+int PT_Front = 0;  // for queue
+
+int* free_frames_list;
+
+std::vector<int> Page_Table(10);
+
+
+// for use of quantom pages
+typedef struct file_struct {
+  FILE fp*;
+  int finish;
+};
+
+// create our structure for the file
+struct file_struct *newStruct (char* text){
+
+  struct file_struct *retVal = malloc(sizeof(struct file_struct));
+  if(retVal == NULL){
+    return NULL;
+  }
+  retVal->fp = fopen(text, "r");
+  retVal->finish = 0;
+
+  return retVal;
+
+
+}
+
+/*
+* Update the PT
+* if free mem, append to PT
+* Else use LRU OR FIFO
+*/
+int updatePT(int PN){
+  int HIT;
+
+  // STACK
+  if(fl_Mode == LRU){
+
+  }
+
+  // QUEUE
+  else if(fl_Mode == FIFO){
+
+  }
+
+  return HIT;
+}
+
+/*
+* Update using LRU
+*/
+
+int updateTLB(int PN){
+    int HIT;
+
+      //search
+
+
+
+
+      //FOUND
+
+
+      //PAGE # () | Frame # () | ASID PID | V/I
+      if(fl_Mode == GLOBAl_MODE){
+
+        // Not Found
+        top++;
+        stack[top] = PN;
+
+      }
+
+      //PAGE # () | Frame # ()| V/I
+      else if(fl_Mode == LOCAL_MODE) {
+
+        // Not Found
+        top++;
+        stack[top] = PN;
+
+
+      }
+
+    return HIT;
+}
+
+
+
+// clear the struct
+void delVector (struct file_struct *struct){
+  if( struct != NULL){
+    fclose(retVal->fp);
+    free(struct);
+  }
+}
 
 
 // read in a pgsize, tlbentries { g| p} quantom physpages { f|l } tracefiles
@@ -13,11 +113,16 @@ int main(int argc, char *argv[]) {
 
 
   int pgsize = atof(argv[1]);
+
+
   int tlbentries = atof(argv[2]);
+  stack = (int *)malloc(tlbentries * sizeof(int)); // allocate our TLB
+
+
   char* tlbentries_Mode = (char*) malloc(1);
   tlbentries_Mode = argv[3];
 
-  int gp_Mode = 0;
+
 
   if (tlbentries_Mode == 'g'){
     gp_Mode = GLOBAl_MODE;
@@ -35,7 +140,7 @@ int main(int argc, char *argv[]) {
   char* quantom_Mode = (char*)malloc(1);
   quantom_Mode = argv[5];
 
-  int fl_Mode;
+
 
   if (tlbentries_Mode == 'f'){
     fl_Mode = FIFO;
@@ -55,12 +160,70 @@ int main(int argc, char *argv[]) {
   char **traceFiles;
   traceFiles = malloc(traceFileAmount * sizeof(char*));
 
+  file_struct *array = malloc(traceFileAmount * sizeof(struct));
+
+
+  // get the file names and allocate the struct
   for(x =0; x < traceFileAmount; x++){
     traceFiles[x] = malloc(strlen(argv[x+6]) * sizeof(char));
     strcpy(traceFiles[x],argv[x+6]);
 
+    // create the structs
+    file_struct *struct = newStruct(argv[x+6]);
+    array[x] = file_struct;
+
+
   }
 
+
+  // read
+  int finish = 0 ;
+  int HIT =0, PT_HIT=0;
+  // loop until end of last tracefile
+  x = 0;
+
+  while(1){
+
+    for(i =0; i < quantom_Pages; i++){
+
+      int PN = read_file(array[x]->fp); // read the file
+
+      Hit = TLB(PN);
+
+      if(HIT){
+        continue;
+      }
+      else{
+        PT_HIT = PT(PN);
+      }
+
+      // start of PT
+      if(PT_HIT){
+        updateTLB();
+      }
+
+      else{
+        updatePT();
+        updateTLB();
+
+      }
+
+    }
+
+    // flush the TLB
+    if(gp_Mode == LOCAL_MODE){
+
+    }
+
+    x++;
+    if(array[x]->finish == 1){
+      x++;
+    }
+
+    x = x%traceFileAmount; // loop back around
+
+
+  }
 
 
 
