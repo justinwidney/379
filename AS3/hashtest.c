01bd5f713e70d125f7db948db8593136be711199
Justin Widney <jwidney@cs.ualberta.ca>
2017-04-09 14:14:01 -0600

    more work on hashing

--- AS3/hashtest.c
+++ AS3/hashtest.c
@@ -13,34 +13,7 @@ struct page_Table_Entry{
 };
 
 
-/* page_Table_Entry **pageTable, **top;
-
-void createHashTable(){
-
-  int i;
-
-  pageTable = page_Table_Entry*[25];
-  top = page_Table_Entry*[25];
-
-  for(i=0; i< 25; i++){
-    pageTable[i] = NULL;
-    top[i] = NULL;
-  }
-
-
-
-}
-
-// may need improvement
-int HashFunc(int PN){
-  return PN % 25;
-}
-*/
-
 struct page_Table_Entry* head;  // our first entry
-
-
-
 
 
 struct page_Table_Entry* createNewNode(int PT, int FrameNumber){
@@ -88,6 +61,7 @@ void InsertAtTail(int PT, int FrameNumber) {
 
 	struct page_Table_Entry* temp = head;
 	struct page_Table_Entry* newNode = createNewNode(PT, FrameNumber);
+
 	if(head == NULL) {
 		head = newNode;
 		return;
@@ -98,6 +72,68 @@ void InsertAtTail(int PT, int FrameNumber) {
 
 
 }
+
+
+
+
+int HashFunc(int PN){
+  return PN % 25;
+}
+
+// allocate some memory for our hashtable
+struct page_Table_Entry **createHashTable(int hashSize){
+  struct page_Table_Entry **table = malloc( sizeof(struct page_Table_Entry)*hashSize);
+  return table;
+}
+
+void hashInsert(struct page_Table_Entry **hashtable,  int PN, int FN){
+int arrayIndex = HashFunc(PN);
+
+  if(hashtable[arrayIndex]!= NULL){
+  InsertAtTail(PN,FN);
+  }
+  else{
+    printf("creating new node\n");
+    struct page_Table_Entry* newNode = createNewNode(PN, FN);
+    InsertAtTail(PN,FN);
+    hashtable[arrayIndex] = newNode;
+  }
+
+}
+
+struct page_Table_Entry *lookup(struct page_Table_Entry **hashtable, int PN){
+  int arrayIndex = HashFunc(PN);
+
+  struct page_Table_Entry* newNode;
+
+
+  struct page_Table_Entry* temp = head;
+
+  if(head == NULL) {
+		head = newNode;
+		return newNode;
+	}
+
+	while(temp->next != NULL) {
+
+  if(temp->PageNumber == PN){
+    printf("found it\n");
+    return temp;
+  }
+
+  temp = temp->next;
+  }
+
+  return NULL;
+
+}
+
+
+
+
+
+
+
 
 
 
@@ -127,12 +163,21 @@ int main(int argc, char *argv[]) {
   int PageNumber, value;
   head = NULL;
 
-  InsertAtTail(2,3);
-  InsertAtTail(4,5);
-  InsertAtTail(5,6);
-  InsertAtHead(1,7);
+  //InsertAtTail(2,3);
+  //InsertAtTail(4,5);
+  //InsertAtTail(5,6);
+  //InsertAtHead(1,7);
+
+  struct page_Table_Entry** table = createHashTable(25);
+
+  hashInsert(table, 1,2);
+  hashInsert(table, 2,3);
+  hashInsert(table, 3,4);
   Print();
 
+  struct page_Table_Entry* temp = lookup(table, 2);
+
+  printf("found: %d\n", temp->PageNumber );
 
 
 
